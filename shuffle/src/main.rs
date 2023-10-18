31fddf690d2d84a01b60047f70a8dc9cb6429aab
omar0ali <bajunaidomar@gmail.com>
2023-10-18 00:38:07 -0400

    Added Shuffle

--- shuffle/src/main.rs
+++ shuffle/src/main.rs
@@ -2,10 +2,8 @@ use std::env::args;
 use rand::Rng; // 0.8.5
 fn main() {
     let mut args: Vec<String> = args().skip(1).collect(); // Skip the executable name
-    let len = args.len();
-
-    for i in 0..len {
-        let random_index = rand::thread_rng().gen_range(0..len);
+    for i in 0..args.len() {
+        let random_index: usize = rand::thread_rng().gen_range(0..args.len());
         args.swap(i, random_index);
     }
 
