a6f931d208b9654676124e53a7f512ef5f03e199
omar0ali <bajunaidomar@gmail.com>
2023-10-18 00:46:28 -0400

    Updated shuffle, Added usage instructions.

--- shuffle/src/main.rs
+++ shuffle/src/main.rs
@@ -2,8 +2,12 @@ use std::env::args;
 use rand::Rng; // 0.8.5
 fn main() {
     let mut args: Vec<String> = args().skip(1).collect(); // Skip the executable name
+    if args.len() < 2 {
+        println!("Usage: shuffle_app <item> <item> ...\nn > 2");
+        return;
+    }
     for i in 0..args.len() {
-        let random_index: usize = rand::thread_rng().gen_range(0..args.len());
+        let random_index: usize = rand::thread_rng().gen_range(0..args.len()); //Getting the size of arguments.
         args.swap(i, random_index);
     }
 
